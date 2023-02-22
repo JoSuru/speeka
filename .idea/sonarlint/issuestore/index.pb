3293c8a15be02bdd123ca497b07f27767e4b11ad
Jonathan Suru <jonathansuru@icloud.com>
2023-02-22 23:51:09 +0100

    init frontend

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+>
+test_main.http,c/b/cbdc4133ce020dc49f7a0e5509c9dff899a94f38
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+:
+
+LICENSE.md,4/a/4a8486805915245bbbe2e3c8c1a9d680c6843a82
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,51 @@
-import  streamlit as st
+import streamlit as st
+from annotated_text import annotated_text
+from streamlit_modal import Modal
+
+
+st.set_page_config("Speeka", "💬", layout="wide")
+
+with open('src/style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([1, 3, 1])
+
+with col1:
+    st.write("# Import  voice")
+    with st.empty():
+        file = st.file_uploader("Upload file", type=["wav", "mp3"])
+    col_center1, button, col_center2 = st.columns([1, 1, 1])  # center button
+    with button:
+        if st.button("Transcribe"):
+            st.write("Importing")
+
+with col2:
+    st.write("# Transcribe")
+    col_center1, text_transcribe, col_center2 = st.columns([1, 30, 1])  # center text
+    with text_transcribe:
+        annotated_text(
+            "This ",
+            ("is", "verb", "#8ef"),
+            " some ",
+            ("annotated", "adj", "#faa"),
+            ("text", "noun", "#afa"),
+            " for those of ",
+            ("you", "pronoun", "#fea"),
+            " who ",
+            ("like", "verb", "#8ef"),
+            " this sort of ",
+            ("thing", "noun", "#afa"),
+        )
+    if st.button("Edit"):
+        st.write("Editing")
+
+modal = Modal("Demo Modal", "Abc")
+if open_modal := st.button("Open"):
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
